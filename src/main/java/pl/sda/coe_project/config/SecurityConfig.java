ab9dadfb22a5db7a4f818ee48a32a4edb81be0bc
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-18 12:13:14 +0100

    updated SecurityConfig class

--- src/main/java/pl/sda/coe_project/config/SecurityConfig.java
+++ src/main/java/pl/sda/coe_project/config/SecurityConfig.java
@@ -1,8 +1,42 @@
 package pl.sda.coe_project.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .withUser("admin").password("{noop}admin").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/myForm");
+
+        http.csrf().disable();
+
+    }
 }
