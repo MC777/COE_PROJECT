8167d07c5735498a71866e4b5303a8e234c3b6b0
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-18 11:48:34 +0100

    ExchangeController update

--- src/main/java/pl/sda/coe_project/controller/ExchangeController.java
+++ src/main/java/pl/sda/coe_project/controller/ExchangeController.java
@@ -1,4 +1,17 @@
 package pl.sda.coe_project.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ExchangeController {
+    @GetMapping("/")
+    public String getMainView() {
+        return "login.jsp";
+    }
+
+    @GetMapping("/myForm")
+    public String getProtectedView() {
+        return "main.jsp";
+    }
 }
