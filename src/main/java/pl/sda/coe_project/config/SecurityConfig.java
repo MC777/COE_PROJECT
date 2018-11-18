7403d1d306cf22c23c2c17f7ca57ebf816d2f8d3
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-18 14:39:02 +0100

    added method allowUrlEncodedSlashHttpFirewall in SecurityConfig class

--- src/main/java/pl/sda/coe_project/config/SecurityConfig.java
+++ src/main/java/pl/sda/coe_project/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package pl.sda.coe_project.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 import javax.sql.DataSource;
 
@@ -38,5 +41,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
 
+    }
+
+    @Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall(){
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedSlash(true);
+        return firewall;
     }
 }
