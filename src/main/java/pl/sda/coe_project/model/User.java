50ef03f1562a5f8e3c7429c84d1da5aa6c39569d
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-18 14:31:27 +0100

    update User class

--- src/main/java/pl/sda/coe_project/model/User.java
+++ src/main/java/pl/sda/coe_project/model/User.java
@@ -3,10 +3,20 @@ package pl.sda.coe_project.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+
 }
