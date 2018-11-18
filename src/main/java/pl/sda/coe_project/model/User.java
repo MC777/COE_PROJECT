2da1245911a3ff8ae81d098410fca24849353ddc
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-18 14:14:10 +0100

    changed location of web files

--- src/main/java/pl/sda/coe_project/model/User.java
+++ src/main/java/pl/sda/coe_project/model/User.java
@@ -1,4 +1,12 @@
 package pl.sda.coe_project.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
 public class User {
 }
--- src/main/java/pl/sda/coe_project/model/UserRole.java
+++ src/main/java/pl/sda/coe_project/model/UserRole.java
@@ -1,4 +1,14 @@
 package pl.sda.coe_project.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table
 public class UserRole {
 }
