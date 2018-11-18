ff6a1779d71210da5ec9f572ae757f0c5863b9fb
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-18 10:33:09 +0100

    initial state

---
+++ src/main/java/pl/sda/coe_project/CoeProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.coe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoeProjectApplication.class, args);
+    }
+}
