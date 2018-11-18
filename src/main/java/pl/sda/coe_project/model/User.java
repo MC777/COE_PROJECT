6802b8100401b153dc786bc3b05177018abb8fb9
mat03 <fiolek.m@gmail.com>
2018-11-18 14:59:34 +0100

    Merge branch 'master' of /home/mat03/Desktop/exchange_project/COE_PROJECT with conflicts.

--- src/main/java/pl/sda/coe_project/model/User.java
+++ src/main/java/pl/sda/coe_project/model/User.java
@@ -2,21 +2,68 @@ package pl.sda.coe_project.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
-public class User {
-
+public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NotEmpty
+
     private String username;
-    @NotEmpty
+
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return convertAuthorities();
+
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    private Set<GrantedAuthority> convertAuthorities(Set<UserRole> userRoles) {
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (UserRole role : userRoles) {
+
+            authorities.add(new SimpleGrantedAuthority(role.getRole()));
+
+        }
+
+        return authorities;
+
+    }
 }
--- src/main/java/pl/sda/coe_project/model/UserRole.java
+++ src/main/java/pl/sda/coe_project/model/UserRole.java
@@ -2,13 +2,13 @@ package pl.sda.coe_project.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
-@Table
-public class UserRole {
+public class UserRole{
+    private Long user_role_id;
+    private String username;
+    private String authority;
 }
