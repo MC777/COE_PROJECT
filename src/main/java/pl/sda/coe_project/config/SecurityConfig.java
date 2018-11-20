1c9236649ca3e772260adf421f37a01f1704a0af
mat03 <fiolek.m@gmail.com>
2018-11-20 23:53:18 +0100

    change securityconfig

--- src/main/java/pl/sda/coe_project/config/SecurityConfig.java
+++ src/main/java/pl/sda/coe_project/config/SecurityConfig.java
@@ -1,21 +1,32 @@
 package pl.sda.coe_project.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
+import pl.sda.coe_project.service.CustomUserDetailsService;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableWebMvcSecurity
+@ComponentScan(basePackageClasses = CustomUserDetailsService.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final DataSource dataSource;
+
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     public SecurityConfig(DataSource dataSource) {
