07020521345c9ab52eec255eeca1b32a20562194
mat03 <fiolek.m@gmail.com>
2018-11-20 23:51:25 +0100

    add query to IUserRoleRepository

--- src/main/java/pl/sda/coe_project/repository/IUserRoleRepository.java
+++ src/main/java/pl/sda/coe_project/repository/IUserRoleRepository.java
@@ -1,10 +1,14 @@
 package pl.sda.coe_project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.sda.coe_project.model.UserRole;
 
+import java.util.List;
+
 @Repository
 public interface IUserRoleRepository extends JpaRepository<UserRole, Long> {
-    UserRole finByAuthority(String authority);
+    @Query("select a.role from UserRole a, User b where b.userName=?1 and a.userid=b.userId")
+    public List<String> findRoleByUserName(String authority);
 }
