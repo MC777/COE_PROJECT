ef6db4a5b133107c60977744a047d54774e3f8de
mat03 <fiolek.m@gmail.com>
2018-11-23 22:16:08 +0100

    cryptocurrency chage file name, add description onetomany

--- src/main/java/pl/sda/coe_project/model/Cryptocurrency.java
+++ src/main/java/pl/sda/coe_project/model/Cryptocurrency.java
@@ -6,16 +6,17 @@ import lombok.Setter;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "cryptocurrency")
-public class CryptoCurrency {
+public class Cryptocurrency {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_cryptocurrency")
-    private Long cryptocurrencyId;
+    private Long id;
 
     @NotNull
     @Size(max = 10)
@@ -33,4 +34,7 @@ public class CryptoCurrency {
 
     @Column(name = "enabled")
     private Boolean enabled;
+
+    @OneToMany(mappedBy = "cryptocurrency")
+    private List<CryptocurrencyWallet> cryptocurrencyWalletList;
 }
