9f00feb0445e3bc85c41aeecf90ba424215a7cf1
mat03 <fiolek.m@gmail.com>
2018-11-23 22:14:05 +0100

    add model orewallet

---
+++ src/main/java/pl/sda/coe_project/model/OreWallet.java
@@ -0,0 +1,32 @@
+package pl.sda.coe_project.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ore_wallet")
+public class OreWallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_ore_wallet")
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "ore_id")
+    private Ore ore;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @NotNull
+    private BigDecimal quantity;
+}
