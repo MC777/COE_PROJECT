5a084e3e9f2c88b8c96d740911691351a2e7b2ed
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-25 10:24:39 +0100

    update of ExchangeController

--- src/main/java/pl/sda/coe_project/controller/ExchangeController.java
+++ src/main/java/pl/sda/coe_project/controller/ExchangeController.java
@@ -1,10 +1,23 @@
 package pl.sda.coe_project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.sda.coe_project.model.User;
+import pl.sda.coe_project.service.UserService;
 
 @Controller
 public class ExchangeController {
+
+    private final UserService userService;
+
+    @Autowired
+    public ExchangeController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String getMainView() {
         return "index.jsp";
@@ -12,16 +25,17 @@ public class ExchangeController {
 
     @GetMapping("/loginForm")
     public String getLoginView() {
-        return "/login.jsp";
+        return "login.jsp";
     }
 
     @GetMapping("/registerForm")
     public String getRegisterView() {
-        return "/register.jsp";
+        return "register.jsp";
     }
 
-    @GetMapping("/myLogout")
-    public String getLogoutView() {
-        return "/register.jsp";
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.addUser(user);
+        return "redirect:/";
     }
 }
