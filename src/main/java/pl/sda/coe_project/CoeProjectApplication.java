f018cf1faed859ea042f7da914a044b5627932e2
Marek Ciesla <ciesla.marek@gmail.com>
2018-11-25 10:28:02 +0100

    added Bean for PasswordEncoder

--- src/main/java/pl/sda/coe_project/CoeProjectApplication.java
+++ src/main/java/pl/sda/coe_project/CoeProjectApplication.java
@@ -2,11 +2,19 @@ package pl.sda.coe_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CoeProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoeProjectApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder getEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
