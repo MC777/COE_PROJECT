485feb4309a877efffaa3a40baef6496bef48028
mat03 <fiolek.m@gmail.com>
2018-11-25 12:08:15 +0100

    change classes name and package place

--- src/main/java/pl/sda/coe_project/exchanger/NbpExchangeGoldPrice.java
+++ src/main/java/pl/sda/coe_project/exchanger/NbpExchangeGoldPrice.java
@@ -1,4 +1,4 @@
-package pl.sda.coe_project.connector;
+package pl.sda.coe_project.exchanger;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @Component
 @Getter
 @Setter
-public class RequesSampleGoldPriceDataResponse {
+public class NbpExchangeGoldPrice {
     private String data;
     private BigDecimal cena;
 }
