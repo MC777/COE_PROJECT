4cfc9b7c4ac95ecf3352257275efb51481578a9e
Marek Ciesla <ciesla.marek@gmail.com>
2018-12-01 14:51:47 +0100

    update of UserService with main content

--- src/main/java/pl/sda/coe_project/service/UserService.java
+++ src/main/java/pl/sda/coe_project/service/UserService.java
@@ -3,32 +3,46 @@ package pl.sda.coe_project.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.sda.coe_project.assembler.UserAssembler;
+import pl.sda.coe_project.assembler.UserInfoAssembler;
 import pl.sda.coe_project.model.User;
+import pl.sda.coe_project.model.UserInfo;
 import pl.sda.coe_project.model.UserRole;
+import pl.sda.coe_project.model.dto.UserDto;
 import pl.sda.coe_project.repository.IUserRepository;
 import pl.sda.coe_project.repository.IUserRoleRepository;
 
 @Service
 public class UserService {
 
-    private static final String DEFAUL_ROLE ="ROLE_USER";
+    private static final String DEFAUL_ROLE = "ROLE_USER";
 
     private final IUserRepository userRepository;
     private final IUserRoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final UserAssembler userAssembler;
+    private final UserInfoAssembler userInfoAssembler;
+
     @Autowired
     public UserService(IUserRepository userRepository, IUserRoleRepository roleRepository,
-        PasswordEncoder passwordEncoder){
+                       PasswordEncoder passwordEncoder, UserAssembler userAssembler, UserInfoAssembler userInfoAssembler) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userAssembler = userAssembler;
+        this.userInfoAssembler = userInfoAssembler;
     }
 
-    public void addUser(User user){
+    public void addUser(UserDto userDto) {
+        User user = userAssembler.fromUserDto(userDto);
+        UserInfo userInfo = userInfoAssembler.fromUserInfoDto(userDto);
+
         UserRole defaultRole = roleRepository.findByRole(DEFAUL_ROLE);
         user.getUserRoles().add(defaultRole);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setUserInfo(userInfo);
+
         userRepository.save(user);
     }
 }
