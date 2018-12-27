4be0e0bc0d3c9aacf9e57fc487cc5d6cf4d6a037
Marek Ciesla <ciesla.marek@gmail.com>
2018-12-27 21:47:07 +0100

    added access fo navbar for each groups

--- src/main/java/pl/sda/coe_project/config/SecurityConfig.java
+++ src/main/java/pl/sda/coe_project/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/css/**","/js/**","/resources/**", "/exchange", "/registerForm", "/register").permitAll()
+                .antMatchers("/", "/css/**", "/js/**", "/resources/**", "/exchange", "/registerForm", "/register", "/loginForm", "/contact", "/gold", "/cryptocurrency").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
