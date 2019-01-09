da1b0f2f43a71122feb26b565b1b9550093af8c7
Marek Ciesla <ciesla.marek@gmail.com>
2019-01-09 23:18:01 +0100

    refactor of code for GoldController

--- src/main/java/pl/sda/coe_project/controller/GoldController.java
+++ src/main/java/pl/sda/coe_project/controller/GoldController.java
@@ -2,14 +2,14 @@ package pl.sda.coe_project.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import pl.sda.coe_project.model.GoldRatesResult;
 import pl.sda.coe_project.service.GoldService;
 import pl.sda.coe_project.service.UserService;
 
-@RestController
+@Controller
 public class GoldController {
     private final UserService userService;
     private final GoldService goldService;
@@ -19,13 +19,13 @@ public class GoldController {
         this.goldService = goldService;
     }
 
-    @GetMapping("/gold")
-    public String getGoldView() {
-        return "gold.jsp";
-    }
-
     @GetMapping("/gold/prices")
     public ResponseEntity<GoldRatesResult> getLastGoldPrice(Model model) {
         return new ResponseEntity<>(goldService.getGoldPricesForPeriod(), HttpStatus.OK);
+    }
+
+    @GetMapping("/gold")
+    public String getGoldView() {
+        return "gold.jsp";
     }
 }
