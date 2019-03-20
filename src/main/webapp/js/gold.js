c113b97b0e391bd80ba8ccd751003f16713278ad
Marek Ciesla <ciesla.marek@gmail.com>
2019-03-20 19:37:04 +0100

    label Sessions renamed for Price

--- src/main/webapp/js/gold.js
+++ src/main/webapp/js/gold.js
@@ -19,7 +19,7 @@ $.ajax({
             data: {
                 labels: result.dates,
                 datasets: [{
-                    label: "Sessions",
+                    label: "Price",
                     lineTension: 0.3,
                     backgroundColor: "rgba(2,117,216,0.2)",
                     borderColor: "rgba(2,117,216,1)",
